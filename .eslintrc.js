2a83e469c094ca2ddf2cde73db48cf0041e4cf68
Martin Oppitz <github@martinoppitz.com>
2023-01-04 04:14:06 +0100

    chore: configure nightwatch

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,16 @@ const config = require('@leanup/stack/.eslintrc');
 
 config.overrides = config.overrides || [];
 config.overrides.push({
-  extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended'],
-  files: ['**/*.tsx'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  rules: {
-    'react/no-unused-state': 'error',
-  },
+	extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended'],
+	files: ['**/*.tsx'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	rules: {
+		'react/no-unused-state': 'error',
+	},
 });
 
 config.plugins = config.plugins || [];
@@ -19,9 +19,9 @@ config.plugins.push('react');
 config.plugins.push('jsx-a11y');
 
 config.settings = {
-  react: {
-    version: 'detect',
-  },
+	react: {
+		version: 'detect',
+	},
 };
 
 module.exports = config;
